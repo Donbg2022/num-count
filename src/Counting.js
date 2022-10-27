f7ceeb6335b5753f16389ff593557bc0d3f03cae
Donovan <donovangriffin04@gmail.com>
2022-10-26 18:52:04 -0700

    created a number counter using react components and hooks

---
+++ src/Counting.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+    
+    const [count, setCount] = useState(0)
+
+    const increase = () => setCount(count + 1)
+    const decrease = () => setCount(count - 1)
+    const reset = () => setCount(0)
+
+    return ( 
+        <div className="countercontainer">
+            <h1>Button counter</h1>
+            <button onClick={increase}>increase</button>
+            <button onClick={reset}>reset</button>
+            <button onClick={decrease}>decrease</button>
+            <p>the button click amount is {count}</p>
+        </div>
+     );
+     
+}
+ 
+export default Counter;
